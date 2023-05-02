57fe29dc0a9f4309ddcc6d26ba754948a359ba7d
Milhan Osman <milhanomarosman@gmail.com>
2023-05-02 19:20:53 +0300

    home page refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,41 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import Sections from "../components/Sections";
-import { now_playing, popular, top_rated, upcoming } from "../mock/movieData";
+import { apiKey } from "../mock/constants";
 
 const Home = () => {
+  const [allData, setAllDate] = useState([]);
+  const movietypes = ["now_playing", "popular", "top_rated", "upcoming"];
+  const movietypeSyntax = ["Now Playing", "Popular", "Top Rated", "Upcoming"];
+  const getMovieTypeData = async (type) => {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}`
+    );
+    const data = await res.json();
+    return data;
+  };
+  const fetchData = useCallback(async () => {
+    const myPromises = movietypes.map(
+      async (item) => await getMovieTypeData(item)
+    );
+    const res = await Promise.all(myPromises);
+    setAllDate(res.map((item, i) => {
+      return {
+        name: movietypeSyntax[i],
+        results: item.results
+      };
+    }))
+  }, []);
+
+  useEffect(() => {
+    fetchData().then().catch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <NavBar />
-      <Sections allData={[now_playing, popular, top_rated, upcoming]} />
+      <Sections allData={allData} />
     </>
   );
 };
