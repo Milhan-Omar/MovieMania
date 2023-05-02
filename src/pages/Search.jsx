bcdab5af237fd1acc6ef10ac4fbde488c40154be
Milhan Osman <milhanomarosman@gmail.com>
2023-05-02 16:17:19 +0300

    create search page

---
+++ src/pages/Search.jsx
@@ -0,0 +1,44 @@
+// import React from 'react'
+
+import { useParams } from "react-router-dom";
+import { apiKey } from "../mock/constants";
+
+import SmallMovieCard from "../components/SmallMovieCard";
+import { useEffect, useState } from "react";
+
+const Search = () => {
+  const { name } = useParams();
+  const [searchedData, setSearchedData] = useState([]);
+
+  const fetchData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${name}&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setSearchedData(data.results));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="collection-main">
+      <h1 className="collection-name">Search results for {`"${name}"`}</h1>
+      <div className="sm-cards-collections">
+        {searchedData.map((data) => (
+          <SmallMovieCard
+            key={data.id}
+            title={data.original_title}
+            imgUrl={data.poster_path}
+            rating={data.vote_average}
+            id={data.id}
+            date={data.release_date}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
